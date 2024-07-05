eaafdb9afa5e1e9f3afabc5cdc692f5ae79e915b
mclouis14 <louis.officialz@gamil.com>
2024-07-05 06:19:50 -0700

    0x00-python_variable_annotations

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Complex types - list of floats"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Sums the elements in the input_list of floats."""
+    return float(sum(input_list))
