e7f8a14d77e6251c83f6ff46126f33da06004e18
mclouis14 <louis.officialz@gamil.com>
2024-07-05 06:51:00 -0700

    0x00-python_variable_annotations

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Complex types - functions"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns a function that multiplies a float by the given multiplier."""
+    return lambda x: x * multiplier
+
